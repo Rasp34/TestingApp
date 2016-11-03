db020ea604398272308fdd6201ed8bfc7237d6b6
Rasp347 <raspberry347@gmail.com>
2016-11-03 15:47:43 +0300

    add summer function

--- app/src/main/java/com/develop/art_me/testinggit/MainActivity.java
+++ app/src/main/java/com/develop/art_me/testinggit/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println(summer(2, 3));
+    }
+
+    public int summer(int i, int c){
+        return i + c;
     }
 }
